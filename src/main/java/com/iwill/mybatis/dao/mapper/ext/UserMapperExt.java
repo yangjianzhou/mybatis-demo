4ba0d322a1d6b18f32c62fe3e759e50138018d30
iwill <yangjianzhouctgu@126.com>
2019-04-04 22:16:51 +0800

    添加配置

--- src/main/java/com/iwill/mybatis/dao/mapper/ext/UserMapperExt.java
+++ src/main/java/com/iwill/mybatis/dao/mapper/ext/UserMapperExt.java
@@ -3,6 +3,7 @@ package com.iwill.mybatis.dao.mapper.ext;
 import com.iwill.mybatis.dao.mapper.gen.UserMapper;
 import com.iwill.mybatis.dao.model.UserDTO;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public interface UserMapperExt extends UserMapper {
 
     int updateByName(@Param("name") String name ,@Param("age") int age);
 
+    @Update("update cnt_user")
     int update(@Param("columnName") String columnName ,@Param("name") String name,@Param("age") int age);
 
 }
